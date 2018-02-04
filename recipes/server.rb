60c24ea2ba6d810e28ebfa290f8976e3b938a1d0
vagrant <vagrant@localhost.localdomain>
2018-02-04 20:46:21 +0000

    added remote file functionality that download a file and make it available for the recipe

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,4 +1,14 @@
-package 'httpd'
+package 'httpd' do
+  action :install
+end
+
+#cookbook_file '/var/www/html/index.html' do
+#  source "index.html"
+#end
+
+remote_file '/var/www/html/lion.jpg' do
+  source "http://animals.sandiegozoo.org/sites/default/files/2016-09/animals_hero_lions_0.jpg"
+end
 
 template '/var/www/html/index.html' do
   action :create  
