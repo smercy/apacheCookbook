2ac9e2eb7980693bfe3a321abeed871787485a76
vagrant <vagrant@localhost.localdomain>
2018-02-04 06:49:52 +0000

    pulled nodes attributes ip address and hostname and pushed them to the default page index.html

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,7 +1,10 @@
 package 'httpd'
 
 file '/var/www/html/index.html' do
-  content '<h1> Hello, world! </h1>'
+  content "<h1> Hello, world! </h1>
+           IPAddress: #{node['ipaddress']} 
+           Hostname: #{node['hostname']}
+"
 end
 
 service 'httpd' do
